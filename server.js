c8224e3e71c9f7cce6b6b6de68ab708547537717
Lauren Hale <lauren.m.hale7@gmail.com>
2021-11-04 17:02:56 -0500

    added webpack config, and front end

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 //loads all environment variables from .env
 require('dotenv').config()
 const express = require('express');
+const path = require('path')
 const app = express();
 const mongoose = require('mongoose')
 
@@ -12,12 +13,16 @@ db.once('open', ()=> console.log('Connected to Database'))
 
 //lets our server accept json as a body
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-// const usersRouter = require('./routes/users')
+app.use(express.static(path.join(__dirname, './public')))
+
 const cardsRouter = require('./routes/cards')
-
-// app.use('/users', usersRouter.router)
 app.use('/cards', cardsRouter)
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'))
+  }) 
 
 const port = 3000;
 app.listen(port,() => console.log(`Server listening on port ${port}`));
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const { resolve } = require('path')
+module.exports = {
+    entry: [
+      './app/main.js'
+    ],
+    output: {
+      path: __dirname,
+      filename: './public/bundle.js'
+    },
+    devtool: 'source-map',
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-react'
+            ]
+          }
+        }
+      ]
+    }
+  }
